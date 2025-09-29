83c7eb5502e89bd7db3d74bf1e0e7005b1d34f52
pje8740 <pje8740@cu.ac.kr>
2025-09-30 08:47:00 +0900

    1차 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -8,17 +8,54 @@ function App() {
       content: "안녕하세요",
     },
   ]);
+
   return (
     <>
-      <input />
-      <button>추가</button>
-      <ul>
-        {todo.map((el) => (
-          <li key={el.id}>{el.content}</li>
-        ))}
-      </ul>
+      <TodoInput setTodo={setTodo} />
+      <TodoList todo={todo} setTodo={setTodo} />
     </>
   );
 }
 
+const TodoInput = ({ setTodo }) => {
+  const inputRef = useRef(null);
+  const addTodo = () => {
+    const newTodo = {
+      id: Number(new Date()),
+      content: inputRef.current.value,
+    };
+    setTodo((prev) => [...prev, newTodo]);
+  };
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={addTodo}>추가</button>
+    </>
+  );
+};
+
+const TodoList = ({ todo, setTodo }) => {
+  return (
+    <ul>
+      {todo.map((el) => (
+        <Todo todo={el} setTodo={setTodo} />
+      ))}
+    </ul>
+  );
+};
+
+const Todo = ({ todo, setTodo }) => {
+  return (
+    <li key={todo.id}>
+      {todo.content}
+      <button
+        onClick={() => {
+          setTodo((prev) => prev.filter((el) => el.id !== todo.id));
+        }}
+      >
+        삭제
+      </button>
+    </li>
+  );
+};
 export default App;
